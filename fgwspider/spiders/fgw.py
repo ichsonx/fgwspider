ec45918646462e2491ba951ba287bbb79327fb7c
ichsonx <sonxz@qq.com>
2017-10-20 16:10:45 +0800

    sdf

--- fgwspider/spiders/fgw.py
+++ fgwspider/spiders/fgw.py
@@ -5,22 +5,36 @@ import re
 import json
 import os
 import datetime
+import sys
+import io
+import chardet
 
-class spider1024(scrapy.Spider):
+class fgw(scrapy.Spider):
     name = "fgw"
-    # custom_settings = {
-    #     'ITEM_PIPELINES': {
-    #         'fgwspider.pipelines.FgwspiderPipeline':400
-    #     }
-    # }
-
-    start_urls = ['http://www.szpb.gov.cn/xxgk/qt/tzgg/']
-
-    for i in range(1, 2):
-        start_urls.append('http://www.szpb.gov.cn/xxgk/qt/tzgg/index_'+ str(i) +'.htm')
 
     def __init__(self, *args, **kwargs):
         pass
 
+    def start_requests(self):
+        start_urls = ['http://www.szpb.gov.cn/xxgk/qt/tzgg/index.htm']
+        for i in range(1, 1):
+            start_urls.append('http://www.szpb.gov.cn/xxgk/qt/tzgg/index_' + str(i) + '.htm')
+        for url in start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
     def parse(self, response):
-        print(response.body)
+        #sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')
+        #sys.getdefaultencoding()
+        filename = 'result.txt'
+        xpathmark = '//span[contains(text(), "2017")][contains(@class, "p_bt")]/../@href'
+        xpathmark = '//span[contains(text(), "环保")][contains(@class, "p_bt")]/text()'
+        sel = scrapy.Selector(text=response.body)
+        subSelectors = sel.xpath(xpathmark).extract()
+
+        print(len(subSelectors))
+        with open(filename, 'a+') as f:
+            for it in subSelectors:
+                content = 'http://www.szpb.gov.cn/xxgk/qt/tzgg/' + it.replace("./", "")
+                f.write(content + "\n")
+
+        return None
